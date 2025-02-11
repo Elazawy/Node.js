4f6ab690075642f4bb04f57ce55ef94798e7ec41
Elazawy <ahmedsaadelazawy222@gmail.com>
2025-02-11 16:58:57 +0200

    Reordering

--- Task2/main.js
+++
@@ -1,9 +0,0 @@
-const numbers = [5, 2, 9, 1, 7, 3];
-let large = numbers[0];
-let small = numbers[0];
-for(let i=0; i<5; i++){
-    large = Math.max(large, numbers[i]);
-    small = Math.min(small, numbers[i]);
-}
-console.log(`Largest: ${large}`);
-console.log(`Small: ${small}`);
